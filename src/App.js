21544b2a4f70176eec6871a4aa1e9eb3c2b29bae
bakankaya <batakankarakaya@gmail.com>
2024-01-10 15:49:56 +0300

    Added a new blank page and Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes,Route } from 'react-router-dom';
 import Nav from './components/Nav.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import BookingPage from './components/BookingPage.js';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path='/' element={<Main />}/>
+        <Route path='/booking' element={<BookingPage />}/>
       </Routes>
       <Footer />
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from '../images/littleLemon.png';
 
 const Nav = () => {
     return (
         <div className="Navbar">
             <ul>
-                <li><img src={logo}alt="Little-Lemon-Logo"></img></li>
-                <li><a href="#home">Home</a></li>
+                <li><Link to="/"><img src={logo}alt="Little-Lemon-Logo"></img></Link></li>
+                <li><Link to="/">Home</Link></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#menu">Menu</a></li>
-                <li><a href="#reservations">Resevations</a></li>
+                <li><Link to="/booking">Resevations</Link></li>
                 <li><a href="#order-online">Order Online</a></li>
                 <li><a href="#login">Login</a></li>
             </ul>
